d23bfc0a9cbbfe2fff35def9a561b157a16b80b3
DanutZupcau <danut.zupcau.72@lacetania.cat>
2018-02-14 12:29:52 +0100

    Comunicacio Serie Basica

---
+++ 1.Hello_World/Hello_World.ino
@@ -0,0 +1,29 @@
+/***********************************************************
+**                                                        **
+**                    TÍTOL: Hello World                  **
+**                                                        **
+**                                                        **
+**  NOM: Danut Zupcau                   DATA: 10/01/2018  **
+***********************************************************/
+//******************* INCLUDE  *************************
+
+
+//******************* VARIABLE *************************
+
+
+//******************* SETUP ****************************
+
+void setup()                // run once, when the sketch starts
+{
+ Serial.begin(9600);       // set up Serial library at 9600 bps
+ Serial.println("Hello world!"); // prints Hello world! with brake line
+}
+
+//******************* LOOP *****************************
+
+void loop()   // run over and over again
+{
+          // do nothing
+}
+
+//******************* FUNCIONS **************************
